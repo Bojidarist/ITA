cb128d8ec24ea17a6956881686320198191f4445
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-01-29 21:56:07 +0200

    Added project files

---
+++ ConsoleUI/Bootstrap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Bootstrap
+    {
+        public static void Boot()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
